58385558fc2b00bb580f2ea8d41d0e6aa51b0964
Yagnesh Modh <yagnesh.modh@gmail.com>
2021-09-08 12:06:21 +0530

    commit basic code

---
+++ app.js
@@ -0,0 +1,24 @@
+export const firstName = 'Yagnesh';
+
+export const lastName = 'Modh'
+
+export const fullName = () => {
+    return `${firstName} ${lastName}`;
+}
+
+// const user = {
+//     name: 'Rohit',
+//     age: 32,
+//     gender: 'male'
+// }
+
+// export default user;
+
+
+class App {
+
+};
+
+export default App;
+
+
